618da71485b17ad14efb69704a60a9a7887a7521
TakaK22 <25346710+TakaK22@users.noreply.github.com>
2021-11-12 23:30:13 +0900

    FizzBuzz完成

--- src/html/js/ts/fizzBuzz.test.ts
+++ src/html/js/ts/fizzBuzz.test.ts
@@ -1,5 +1,14 @@
 import { fizzBuzz } from "./fizzBuzz";
 
-test('test', () => {
+test('3の倍数を入力するとFizzを返す', () => {
+    expect(new fizzBuzz().convert(3)).toBe('Fizz');
+});
+test('5の倍数を入力するとBuzzを返す', () => {
+    expect(new fizzBuzz().convert(5)).toBe('Buzz');
+});
+test('15の倍数を入力するとFizzBuzzを返す', () => {
+    expect(new fizzBuzz().convert(15)).toBe('FizzBuzz');
+});
+test('その他の数値を入力すると文字列にして返す', () => {
     expect(new fizzBuzz().convert(1)).toBe('1');
 });
--- src/html/js/ts/fizzBuzz.ts
+++ src/html/js/ts/fizzBuzz.ts
@@ -2,6 +2,13 @@ export class fizzBuzz {
 
     constructor() { }
     convert(num: number) {
-        return '1';
+        if (num % 15 === 0) {
+            return 'FizzBuzz';
+        } else if (num % 3 === 0) {
+            return 'Fizz';
+        } else if (num % 5 === 0) {
+            return 'Buzz';
+        }
+        return num.toString();
     }
 }
